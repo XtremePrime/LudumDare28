f6ccd57a74980d95024f681139267f19b3634324
XtremePrime <super_madalin2006@yahoo.com>
2013-12-14 14:24:37 +0200

    First commit
    Just making the frame

---
+++ src/com/ShadowzGames/LD28/Main.java
@@ -0,0 +1,38 @@
+package com.ShadowzGames.LD28;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+public class Main extends StateBasedGame
+{
+	public static final float WIDTH = 640;
+	public static final float HEIGHT = 480;
+	public static final String TITLE = "";
+	
+	public static final int MAIN_MENU = 0;
+	public static final int MAIN_GAME = 1;
+	
+	public Main(String title) {
+		super(title);
+		
+		//this.addState(new MainMenu(MAIN_MENU));
+		this.addState(new MainGame(MAIN_GAME));
+	}
+ 
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.enterState(MAIN_GAME);
+	}
+ 
+	public static void main(String[] args) throws SlickException{
+		AppGameContainer app = new AppGameContainer(new Main(TITLE));
+ 
+		app.setDisplayMode((int)WIDTH, (int)HEIGHT, false);
+		app.setTargetFrameRate(60);
+		app.setShowFPS(false);
+		app.start();
+  	}
+}
